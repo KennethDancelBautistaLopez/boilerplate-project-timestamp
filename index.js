43b4fa2412086b159b653ee487e0cfd94292b328
knnthzuu? <lopez.kennethdancel.02032003@gmail.com>
2024-05-17 05:13:58 +0000

    commit

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-const { is } = require('express/lib/request');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -23,25 +22,42 @@ app.get("/", function (req, res) {
 // your first API endpoint... 
 app.get("/api/:date?", function (req, res) {
   let date = req.params.date;
+
   let unixDate;
   let dateObj;
+  let utcDate;
 
+  // Test whether the input date is a number
   let isUnix = /^\d+$/.test(date);
 
-  if (isUnix){
+  // If no date specified, use the current date
+  if (!date) {
+    dateObj = new Date();
+  }
+  // If the date is a Unix Timestamp
+  else if (date && isUnix) {
     unixDate = parseInt(date);
-  } else if(!isUnix) {
-    "2024-5-17"
+    dateObj = new Date(unixDate);
+  }
+  // If the date is not a unix time stamp
+  else if (date && !isUnix) {
     dateObj = new Date(date);
-    unixDate = dateObj.getTime();
   }
 
-  res.json({unix: unixDate});
+  if (dateObj.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+  unixDate = dateObj.getTime();
+  utcDate = dateObj.toUTCString();
+
+  res.json({ unix: unixDate, utc: utcDate });
 });
 
 
 
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
